8e7df90c7bcb5c9ae3e25999e071eb44909ec8f7
MohamedAlQadeery <moh9amad1@gmail.com>
2022-10-12 23:10:25 +0300

    Created Domain project and added models

---
+++ Backend/PortfolioApplication/Domain/Models/Project.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImagePath { get; set; }
+        /*
+        Project url if it was deployed (nullable)
+        */
+        public string? Url { get; set; }
+    }
+}
