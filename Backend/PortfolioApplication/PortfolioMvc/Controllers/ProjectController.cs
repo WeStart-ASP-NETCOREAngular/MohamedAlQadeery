fa3f3efcbc53a4c146ecb7fb475ef4966396e4ed
MohamedAlQadeery <moh9amad1@gmail.com>
2022-10-13 23:09:13 +0300

    Create project opreation

--- Backend/PortfolioApplication/PortfolioMvc/Controllers/ProjectController.cs
+++ Backend/PortfolioApplication/PortfolioMvc/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
 ﻿using DAL;
+using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using PortfolioMvc.ViewModels;
 
@@ -7,10 +8,12 @@ namespace PortfolioMvc.Controllers
     public class ProjectController : Controller
     {
         private readonly PortfolioDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProjectController(PortfolioDbContext context)
+        public ProjectController(PortfolioDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -26,7 +29,28 @@ namespace PortfolioMvc.Controllers
         [HttpPost]
         public IActionResult Create(CreateProjectVM createProjectVm)
         {
-            return View();
+            if (!ModelState.IsValid) return View();
+
+            var uploadFolder = Path.Combine(_webHostEnvironment.WebRootPath, "Images");
+            var uniqueName = Guid.NewGuid().ToString() + Path.GetExtension(createProjectVm.Image.FileName);
+            // ->0f8fad5b-d9cb-469f-a165-70867728950e.jpg
+
+            var filePath = Path.Combine(uploadFolder, uniqueName);
+
+            //webserver/Images/0f8fad5b-d9cb-469f-a165-70867728950e.jpg
+            createProjectVm.Image.CopyTo(new FileStream(filePath, FileMode.Create));
+
+            _context.Projects.Add(new Project {
+                Title=createProjectVm.Title,
+                Url = createProjectVm.Url,
+                ImagePath = uniqueName
+            });
+
+            _context.SaveChanges();
+
+            TempData["success"] = "Project has been added successfully !";
+            
+            return RedirectToAction(nameof(Create));
         }
     }
 }
