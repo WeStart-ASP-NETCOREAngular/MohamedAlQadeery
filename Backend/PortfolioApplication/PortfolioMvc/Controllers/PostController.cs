8874c41c814ec34b6f8694b51e80a686aa88b5c0
MohamedAlQadeery <moh9amad1@gmail.com>
2022-10-16 02:15:35 +0300

    edit/delete post methods in post controller

--- Backend/PortfolioApplication/PortfolioMvc/Controllers/PostController.cs
+++ Backend/PortfolioApplication/PortfolioMvc/Controllers/PostController.cs
@@ -39,5 +39,50 @@ namespace PortfolioMvc.Controllers
             GenrateTempMessage("success", "Post has been created succesfully");
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var post = _context.Posts.FirstOrDefault(p => p.Id == id);
+
+            if (post == null) return NotFound();
+            return View(new EditPostVM { Id = post.Id, Title = post.Title,Body=post.Body });
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(EditPostVM editPostVM)
+        {
+            if (!ModelState.IsValid) return View();
+            var post = _context.Posts.FirstOrDefault(p => p.Id == editPostVM.Id);
+            if (post == null) return NotFound();
+
+            post.Title = editPostVM.Title;
+            post.Body = editPostVM.Body;
+
+            _context.Posts.Update(post);
+            _context.SaveChangesAsync();
+            GenrateTempMessage("success", "Post has been updated successfully");
+
+            return RedirectToAction(nameof(Edit), new {id=post.Id});
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var post = _context.Posts.FirstOrDefault(p => p.Id == id);
+            if (post == null) return NotFound();
+           
+            _context.Posts.Remove(post);
+            _context.SaveChanges();
+
+            GenrateTempMessage("success", "Post has been deleted successfully");
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
