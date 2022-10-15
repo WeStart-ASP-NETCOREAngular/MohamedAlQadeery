52074fd57437e32fba835242f4d43c90d0d6f687
MohamedAlQadeery <moh9amad1@gmail.com>
2022-10-16 01:51:04 +0300

    added createdAt col in post model/created post controller/index view

--- Backend/PortfolioApplication/Domain/Models/Post.cs
+++ Backend/PortfolioApplication/Domain/Models/Post.cs
@@ -10,5 +10,7 @@ namespace Domain.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
---
+++ Backend/PortfolioApplication/PortfolioMvc/Controllers/PostController.cs
@@ -0,0 +1,31 @@
+﻿using DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioMvc.Controllers
+{
+    public class PostController : BaseController
+    {
+        private readonly PortfolioDbContext _context;
+
+        public PostController(PortfolioDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var posts = _context.Posts.ToList();
+            return View(posts);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+    }
+}
