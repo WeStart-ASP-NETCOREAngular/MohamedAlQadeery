7c0b0b0a27c0f297551e79f448d86d51032f8b9f
MohamedAlQadeery <moh9amad1@gmail.com>
2022-10-20 13:08:59 +0300

    Added Service class library /Genrate token class and interface

---
+++ General/WebApi/BookStoreApi.services/Services/JwtTokenGenerator.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreApi.services.Services
+{
+    public class JwtTokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Generate(IdentityUser user)
+        {
+            // Claims
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name , user.UserName),
+                new Claim(ClaimTypes.Email , user.Email),
+            };
+
+            //SecretKey
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secert"]));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            
+           var token = new JwtSecurityToken(
+              issuer: _configuration["JWT:ValidIssuer"],
+              audience: _configuration["JWT:ValidAudience"],
+              claims: claims,
+              expires: DateTime.Now.AddMinutes(2),
+              signingCredentials: cred);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
