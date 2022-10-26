7e6e2db603d15858a403922caf36b77d309db8c2
MohamedAlQadeery <moh9amad1@gmail.com>
2022-10-26 21:39:29 +0300

    Adding swagger doc in program.cs

--- General/PortfolioApp/PortfolioApp.Api/Program.cs
+++ General/PortfolioApp/PortfolioApp.Api/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using PortfolioApp.Dal;
 using PortfolioApp.Dal.Repositories;
 using PortfolioApp.Domain.Abstraction.Repositories;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +12,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Portfolio Api",
+        Description = "An ASP.NET Core Web API for managing Portfolio",
+       
+    });
+
+    
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
 
 builder.Services.AddDbContext<PortfolioAppDbContext>(options =>
 {
