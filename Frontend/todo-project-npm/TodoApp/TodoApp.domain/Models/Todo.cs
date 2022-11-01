e4e077bd9ae0705e1d8b480882ab9b1a0e7cd31b
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-01 21:50:55 +0200

    Created domain project and created Todo/category models

---
+++ Frontend/todo-project-npm/TodoApp/TodoApp.domain/Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.domain.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+
+        public bool IsCompleted { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
