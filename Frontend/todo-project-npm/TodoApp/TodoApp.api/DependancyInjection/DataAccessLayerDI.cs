81567c70b518e030747e32abdc81b61c32968f7f
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-01 22:58:50 +0200

    Created DAL and made migrations/inject dbcontext in api layer

---
+++ Frontend/todo-project-npm/TodoApp/TodoApp.api/DependancyInjection/DataAccessLayerDI.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.dal;
+
+namespace TodoApp.api.DependancyInjection
+{
+    public static class DataAccessLayerDI
+    {
+
+        public static IServiceCollection AddDataLayer(this IServiceCollection services , IConfiguration config)
+        {
+            services.AddDbContext<TodoAppDbContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("Default"));
+
+            });
+
+            return services;
+        }
+    }
+}
---
+++ Frontend/todo-project-npm/TodoApp/TodoApp.dal/TodoAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.domain.Models;
+
+namespace TodoApp.dal
+{
+    public class TodoAppDbContext : DbContext
+    {
+        public TodoAppDbContext(DbContextOptions<TodoAppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
