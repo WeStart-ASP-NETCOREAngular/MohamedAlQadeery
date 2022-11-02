494eae381d18101913d4395fa805c91ab7f293cc
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-02 17:54:50 +0200

    Adding Category from frontend

--- Frontend/todo-project-npm/TodoApp-Client/src/script.js
+++ Frontend/todo-project-npm/TodoApp-Client/src/script.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 import toastr from "toastr";
-
+import bootstrap from "bootstrap";
 //Buttons click listeners
 
 // Axios
@@ -11,11 +11,13 @@ const BASEURL = "https://localhost:7098";
 
 const categoriesBtn = document.querySelector("#categoriesBtn");
 const todosBtn = document.querySelector("#todosBtn");
+const createCategoryButton = document.querySelector("#createCategoryButton");
+const DisplayCategoryModalButton = document.querySelector(
+  "#DisplayCategoryModalButton"
+);
 
-const tableHead= document.querySelector("#tableHead");
-const tableBody= document.querySelector("#tableBody");
-
-
+const tableHead = document.querySelector("#tableHead");
+const tableBody = document.querySelector("#tableBody");
 
 //categories table
 let categoriesTableHead = ` <tr>
@@ -33,16 +35,16 @@ let todosTableHead = ` <tr>
 
 </tr>`;
 
-categoriesBtn.addEventListener("click",function(event){
-
-    event.preventDefault();
-    tableHead.innerHTML = categoriesTableHead;
-    tableBody.innerHTML = "";
-    axios.get(BASEURL+"/api/category")
-        .then((response) => {
-           
-           const resultData = response.data.map((el,index)=>{
-          return  ` <tr>
+categoriesBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  tableHead.innerHTML = categoriesTableHead;
+  tableBody.innerHTML = "";
+  axios
+    .get(BASEURL + "/api/category")
+    .then((response) => {
+      const resultData = response.data
+        .map((el, index) => {
+          return ` <tr>
             <th scope="col">${++index}</th>
             <th scope="col">${el.name}</th>
             <th scope="col">
@@ -50,27 +52,36 @@ categoriesBtn.addEventListener("click",function(event){
               <a href="#" class="btn btn-primary">Delete</a>
 
             </th>
-          </tr>`
-           }).join(" ");
-           
-    
-        tableBody.innerHTML = resultData;   
-
-
-        }).catch(err=>{
-            console.log(error);
+          </tr>`;
         })
-    
-    
+        .join(" ");
+
+      tableBody.innerHTML = resultData;
+    })
+    .catch((err) => {
+      console.log(error);
+    });
 });
 
-todosBtn.addEventListener("click",function(event){
+todosBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  tableHead.innerHTML = todosTableHead;
+  tableBody.innerHTML = "";
+});
 
-    event.preventDefault();
-    tableHead.innerHTML = todosTableHead;
-    tableBody.innerHTML = "";
-
-
-    
-    
+createCategoryButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  let categoryName = document.querySelector("#categoryName").value;
+  axios
+    .post(BASEURL + "/api/Category", { name: categoryName })
+    .then((response) => {
+      toastr.success(
+        `${response.data.name} category has been created succesfully !`
+      );
+      categoriesBtn.click();
+    })
+    .catch((err) => toastr.error(err.message))
+    .finally(function () {
+      DisplayCategoryModalButton.click();
+    });
 });
