dc7f15b059914f5a4aa5d32bbcc6eed6d751b231
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-09 12:27:16 +0200

    Renaming the success alert class

--- Frontend/angular-alert-task/src/app/app.module.ts
+++ Frontend/angular-alert-task/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SuccessAlert } from './Components/success-alert/success-alert.component';
+import { SuccessAlertComponent } from './Components/success-alert/success-alert.component';
 import { WarningComponent } from './Components/warning-alert/warning-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, WarningComponent, SuccessAlert],
+  declarations: [AppComponent, WarningComponent, SuccessAlertComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
