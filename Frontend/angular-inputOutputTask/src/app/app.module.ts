01f4316ebc01325e9ca58eb87adebcd70f9d429c
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-15 18:22:29 +0200

    created todo components

--- Frontend/angular-inputOutputTask/src/app/app.module.ts
+++ Frontend/angular-inputOutputTask/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { TodoPageComponent } from './todo-page/todo-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoItemComponent,
+    TodoFormComponent,
+    TodoPageComponent
   ],
   imports: [
     BrowserModule
