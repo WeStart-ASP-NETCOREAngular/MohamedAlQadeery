ca3180b178b10b30c7d34f7d186c671215c11597
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-21 14:35:44 +0200

    adding styles for project-list

--- Frontend/protfolio-angular/src/app/app-routing.module.ts
+++ Frontend/protfolio-angular/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectListComponent } from './components/project/project-list/project-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: ProjectListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
