692405a73e93c8fa9417db960ae2875061484ea5
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-21 14:01:28 +0200

    created project interface/project service

--- Frontend/protfolio-angular/src/app/app.module.ts
+++ Frontend/protfolio-angular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectListComponent } from './components/project/project-list/project-list.component';
+import { ProjectItemComponent } from './components/project/project-item/project-item.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PostListComponent } from './components/post/post-list/post-list.component';
+import { PostItemComponent } from './components/post/post-item/post-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectListComponent,
+    ProjectItemComponent,
+    SidebarComponent,
+    PostListComponent,
+    PostItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/protfolio-angular/src/app/services/project.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { IProject } from '../interfaces/IProject';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProjectService {
+  private _project: IProject[] = [
+    {
+      id: 1,
+      title: 'First Project',
+      image: './assets/project-1.png',
+    },
+    {
+      id: 2,
+      title: 'Second Project',
+      image: './assets/project-1.png',
+    },
+    {
+      id: 3,
+      title: 'Thrid Project',
+      image: './assets/project-1.png',
+    },
+  ];
+  constructor() {}
+
+  public GetAllProjects(): IProject[] {
+    return this._project;
+  }
+}
