7b727e7c54991c786faa37c15cf0fa1bb8c545b3
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-21 20:09:56 +0200

    display post info

--- Frontend/protfolio-angular/src/app/app-routing.module.ts
+++ Frontend/protfolio-angular/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostItemComponent } from './components/post/post-item/post-item.component';
 import { PostListComponent } from './components/post/post-list/post-list.component';
 import { ProjectItemComponent } from './components/project/project-item/project-item.component';
 import { ProjectListComponent } from './components/project/project-list/project-list.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'projects', component: ProjectListComponent },
   { path: 'projects/:id', component: ProjectItemComponent },
   { path: 'posts', component: PostListComponent },
+  { path: 'posts/:id', component: PostItemComponent },
 ];
 
 @NgModule({
--- Frontend/protfolio-angular/src/app/components/post/post-item/post-item.component.ts
+++ Frontend/protfolio-angular/src/app/components/post/post-item/post-item.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IPost } from 'src/app/interfaces/IPost';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-post-item',
   templateUrl: './post-item.component.html',
-  styleUrls: ['./post-item.component.css']
+  styleUrls: ['./post-item.component.css'],
 })
 export class PostItemComponent implements OnInit {
-
-  constructor() { }
+  post!: IPost;
+  constructor(
+    private _postService: PostService,
+    private _route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this._route.paramMap.subscribe((para) => {
+      let id = para.get('id')!;
+      this.post = this._postService.GetPostById(+id)!;
+    });
   }
-
 }
