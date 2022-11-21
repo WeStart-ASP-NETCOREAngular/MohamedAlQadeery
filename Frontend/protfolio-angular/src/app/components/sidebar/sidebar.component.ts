0957b62d61dbe17f2a15cac5b6341cd6282997c4
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-21 20:18:53 +0200

    Added post/ projects button in sidebar

--- Frontend/protfolio-angular/src/app/components/sidebar/sidebar.component.ts
+++ Frontend/protfolio-angular/src/app/components/sidebar/sidebar.component.ts
@@ -7,11 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
-  constructor(private _router: Router) {}
+  constructor() {}
 
   ngOnInit(): void {}
-
-  OnClickPosts() {
-    this._router.navigate(['posts']);
-  }
 }
