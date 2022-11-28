01201120f1a1594b992c4b3412121efa3be7cf74
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-28 21:46:38 +0200

    Adding documentation to the project/documentation in category controller

--- Backend/EventWebApp/EventWebApp/Controllers/CategoryController.cs
+++ Backend/EventWebApp/EventWebApp/Controllers/CategoryController.cs
@@ -17,12 +17,27 @@ namespace EventWebApp.Controllers
             _categoryRepository = categoryRepository;
         }
 
+        /// <summary>
+        /// Get All Categories from database
+        /// </summary>
+        /// <returns>
+        /// List of categories
+        /// </returns>
+        /// <response code="200">a list of categories has been retreived successfully </response>
         [HttpGet]
         public async Task<IActionResult> GetAllCategories()
         {
             return Ok(await _categoryRepository.GetAllAsync());
         }
 
+        /// <summary>
+        /// Get a specific category by id
+        /// </summary>
+        /// <returns>
+        /// Returns a category if its found in our database
+        /// </returns>
+        /// <response code="200">Retuens category and its has been found successfully</response>
+        /// <response code="404">Category is not found in our database</response>
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetCategoryById(int id)
@@ -33,7 +48,28 @@ namespace EventWebApp.Controllers
             return Ok(category);
         }
 
-      
+
+
+        /// <summary>
+        /// Add new Category
+        /// </summary>
+        /// <param name="createCategoryDto"></param>
+        /// <returns>
+        ///  returns a newly created category
+        /// </returns>
+        /// /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Post
+        ///     { 
+        ///        "name": "Sports",
+        ///       
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Category has been created successfully</response>
+        /// <response code="400">There was validation error in cateogry values </response>
+
         [HttpPost]
         public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
         {
--- Backend/EventWebApp/EventWebApp/Program.cs
+++ Backend/EventWebApp/EventWebApp/Program.cs
@@ -2,6 +2,8 @@ using EventWebApp.Dal;
 using EventWebApp.Dal.Repositories;
 using EventWebApp.Domain.Abstraction.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +12,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Events Web App Api",
+        Description = "An ASP.NET Core Web API for events web application",
+
+    });
+
+
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
 builder.Services.AddDbContext<EventWebAppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
