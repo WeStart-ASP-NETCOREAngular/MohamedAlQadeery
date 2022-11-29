a30d822b5b492448f9a8d6e1885a08b3770da81a
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-29 22:29:20 +0200

    Creating empty tag reposotiry/tag DTOs

---
+++ Backend/EventWebApp/EventWebApp.Contracts/DTOs/Tag/UpdateTagDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventWebApp.Contracts.DTOs.Tag
+{
+    public class UpdateTagDto
+    {
+        [Required]
+
+        public string Name { get; set; }
+    }
+}
