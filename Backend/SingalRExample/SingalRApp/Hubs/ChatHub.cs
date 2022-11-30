ba60708585cee7adbe8a00f70ca49e00ee42ac15
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-30 12:01:28 +0200

    Creating signalR example with simple send message method

---
+++ Backend/SingalRExample/SingalRApp/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SingalRApp.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessageToAll(string user,string message)
+        {
+            await Clients.All.SendAsync("onMessageRecived", user,message);
+        }
+
+
+    }
+}
