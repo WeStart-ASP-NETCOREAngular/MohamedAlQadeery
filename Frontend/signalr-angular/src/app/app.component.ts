6f71ea57d68af938fc7ace70a1415d12e29503b8
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-30 13:20:29 +0200

    init signlar connection in app

--- Frontend/signalr-angular/src/app/app.component.ts
+++ Frontend/signalr-angular/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from './services/auth.service';
+import * as signalR from '@microsoft/signalr';
+import { HubConnection } from '@microsoft/signalr/dist/esm/HubConnection';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +11,29 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent implements OnInit {
   title = 'signalr-angular';
-
+  messageForm: FormGroup;
+  messages: string[] = [];
+  hubConnection: HubConnection;
   constructor(public _authService: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.messageForm = new FormGroup({
+      message: new FormControl('', Validators.required),
+    });
+
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('https://localhost:7248/chathub')
+      .build();
+
+    this.hubConnection
+      .start()
+      .then(() => {
+        console.log('Connection started successfully');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  SendMessage() {}
 }
