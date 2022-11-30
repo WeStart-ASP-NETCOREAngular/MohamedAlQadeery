a31945e9c8657a5536b149309634a2fe1bdc87a1
MohamedAlQadeery <moh9amad1@gmail.com>
2022-11-30 18:56:11 +0200

    Created send message to specific client method in chathub

--- Backend/SingalRExample/SingalRApp/Hubs/ChatHub.cs
+++ Backend/SingalRExample/SingalRApp/Hubs/ChatHub.cs
@@ -2,13 +2,25 @@
 
 namespace SingalRApp.Hubs
 {
-    public class ChatHub:Hub
+    public class ChatHub : Hub
     {
-        public async Task SendMessageToAll(string user,string message)
+        public async Task SendMessageToAll(string user, string message)
         {
-            await Clients.All.SendAsync("onMessageRecived", user,message);
+            await Clients.All.SendAsync("onMessageRecived", user, message);
+        }
+
+        public async Task SendMessageToClient(string connectionId, Message message)
+        {
+            await Clients.Client(connectionId).SendAsync("onMessageRecived",message);
         }
 
 
+    }
+
+
+    public class Message
+    {
+        public string Username { get; set; }
+        public string Body { get; set; }
     }
 }
