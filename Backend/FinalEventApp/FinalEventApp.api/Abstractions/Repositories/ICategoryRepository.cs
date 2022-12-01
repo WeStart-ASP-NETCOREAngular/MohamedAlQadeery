f2946bb04d08373de94561c6f6cee2caa0291cba
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-02 01:44:13 +0200

    created category repository

---
+++ Backend/FinalEventApp/FinalEventApp.api/Abstractions/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using FinalEventApp.api.Models;
+
+namespace FinalEventApp.api.Abstractions.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetAllCategoriesAsync();
+        Task<Category> GetCategoryByIdAsync(int id);
+        Task<Category> CreateAsync(Category categoryToCreate);
+        Task<Category> UpdateAsync(int id, Category categoryToUpdate);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ Backend/FinalEventApp/FinalEventApp.api/Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,56 @@
+﻿using FinalEventApp.api.Abstractions.Repositories;
+using FinalEventApp.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalEventApp.api.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly EventAppDbContext _context;
+
+        public CategoryRepository(EventAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetAllCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task<Category> CreateAsync(Category categoryToCreate)
+        {
+            await _context.Categories.AddAsync(categoryToCreate);
+            await _context.SaveChangesAsync();
+            return categoryToCreate;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null) return false;
+
+            _context.Categories.Remove(category);
+            return await _context.SaveChangesAsync() > 0;
+
+        }
+
+     
+
+        public async Task<Category> UpdateAsync(int id, Category categoryToUpdate)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null) return category;
+
+            category.Name = categoryToUpdate.Name;
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+    }
+}
