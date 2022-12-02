ccb8bbb62b47e7d6fced715438a92d0c4fb1694c
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-02 20:05:20 +0200

    creating response and request dto for registering user

---
+++ Backend/FinalEventApp/FinalEventApp.api/DTOs/AccountDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalEventApp.api.DTOs
+{
+   public class RegisterUserDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+
+
+    public class RegisterResponseDto
+    {
+        public int RegisterationStatus { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
