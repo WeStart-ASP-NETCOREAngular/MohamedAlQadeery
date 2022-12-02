868045d27029011eb9575aa7ce919dc6e34f64ee
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-02 19:28:41 +0200

    installing mapster

---
+++ Backend/FinalEventApp/FinalEventApp.api/DependancyInjections/MapsterDI.cs
@@ -0,0 +1,21 @@
+﻿using Mapster;
+using MapsterMapper;
+using System.Reflection;
+
+namespace FinalEventApp.api.DependancyInjections
+{
+    public static class MapsterDI
+    {
+        public static IServiceCollection AddMapping(this IServiceCollection services)
+        {
+
+            var config = TypeAdapterConfig.GlobalSettings;
+            config.Scan(Assembly.GetExecutingAssembly());
+
+            services.AddSingleton(config);
+            services.AddScoped<IMapper, ServiceMapper>();
+
+            return services;
+        }
+    }
+}
---
+++ Backend/FinalEventApp/FinalEventApp.api/Mapping/CategoryTagMappingConfig.cs
@@ -0,0 +1,12 @@
+﻿using Mapster;
+
+namespace FinalEventApp.api.Mapping
+{
+    public class CategoryTagMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            
+        }
+    }
+}
