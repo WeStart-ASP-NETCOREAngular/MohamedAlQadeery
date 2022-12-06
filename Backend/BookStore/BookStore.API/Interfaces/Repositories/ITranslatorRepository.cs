937a8a6a9e04b54cfafaf338a03fe1c928384a05
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-06 16:23:32 +0200

    created translator repository

---
+++ Backend/BookStore/BookStore.API/Interfaces/Repositories/ITranslatorRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.API.Models;
+
+namespace BookStore.API.Interfaces.Repositories
+{
+    public interface ITranslatorRepository
+    {
+        Task<List<Translator>> GetAllTranslatorsAsync();
+        Task<Translator> GetTranslatorByIdAsync(int id);
+        Task<Translator> CreateAsync(Translator translatorToCreate);
+        Task<Translator> UpdateAsync(int id, Translator translatorToUpdate);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ Backend/BookStore/BookStore.API/Repositories/TranslatorRepository.cs
@@ -0,0 +1,57 @@
+﻿using BookStore.API.Data;
+using BookStore.API.Interfaces.Repositories;
+using BookStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Repositories
+{
+    public class TranslatorRepository : ITranslatorRepository
+    {
+        private readonly BookStoreDbContext _context;
+
+        public TranslatorRepository(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Translator>> GetAllTranslatorsAsync()
+        {
+            return await _context.Translators.ToListAsync();
+        }
+
+        public async Task<Translator> GetTranslatorByIdAsync(int id)
+        {
+            return await _context.Translators.FindAsync(id);
+        }
+
+
+        public async Task<Translator> CreateAsync(Translator translatorToCreate)
+        {
+            await _context.AddAsync(translatorToCreate);
+            await _context.SaveChangesAsync();
+            return translatorToCreate;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var translator = await _context.Translators.FindAsync(id);
+            if (translator == null) return false;
+
+            _context.Translators.Remove(translator);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+
+
+        public async Task<Translator> UpdateAsync(int id, Translator translatorToUpdate)
+        {
+            var translator = await _context.Translators.FirstOrDefaultAsync(a => a.Id == id);
+            if (translator == null) return null;
+
+            translator.Name = translatorToUpdate.Name;
+            await _context.SaveChangesAsync();
+
+            return translator;
+        }
+
+    }
+}
