6467c47157ad0f0f81c82aa31f6552d1c290ba19
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-06 17:00:08 +0200

    Created Zone Repository

---
+++ Backend/BookStore/BookStore.API/Interfaces/Repositories/IZoneRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.API.Models;
+
+namespace BookStore.API.Interfaces.Repositories
+{
+    public interface IZoneRepository
+    {
+        Task<List<Zone>> GetAllZonesAsync();
+        Task<Zone> GetZoneByIdAsync(int id);
+        Task<Zone> CreateAsync(Zone zoneToCreate);
+        Task<Zone> UpdateAsync(int id, Zone zoneToUpdate);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ Backend/BookStore/BookStore.API/Repositories/ZoneRepository.cs
@@ -0,0 +1,57 @@
+﻿using BookStore.API.Data;
+using BookStore.API.Interfaces.Repositories;
+using BookStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Repositories
+{
+    public class ZoneRepository : IZoneRepository
+    {
+        private readonly BookStoreDbContext _context;
+
+        public ZoneRepository(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Zone>> GetAllZonesAsync()
+        {
+            return await _context.Zones.ToListAsync();
+        }
+
+        public async Task<Zone> GetZoneByIdAsync(int id)
+        {
+            return await _context.Zones.FindAsync(id);
+        }
+
+
+        public async Task<Zone> CreateAsync(Zone zoneToCreate)
+        {
+            await _context.AddAsync(zoneToCreate);
+            await _context.SaveChangesAsync();
+            return zoneToCreate;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var zone = await _context.Zones.FindAsync(id);
+            if (zone == null) return false;
+
+            _context.Zones.Remove(zone);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+
+
+        public async Task<Zone> UpdateAsync(int id, Zone zoneToUpdate)
+        {
+            var zone = await _context.Zones.FirstOrDefaultAsync(a => a.Id == id);
+            if (zone == null) return null;
+
+            zone.Name = zoneToUpdate.Name;
+            await _context.SaveChangesAsync();
+
+            return zone;
+        }
+    }
+}
