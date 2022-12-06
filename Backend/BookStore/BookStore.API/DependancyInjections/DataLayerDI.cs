997a381eb75e0f7b024a7e34e9a081a559a0f155
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-06 14:00:42 +0200

    adding dbcontext in program.cs

---
+++ Backend/BookStore/BookStore.API/DependancyInjections/DataLayerDI.cs
@@ -0,0 +1,34 @@
+﻿using BookStore.API.Data;
+using BookStore.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.DependancyInjections
+{
+    public static class DataLayerDI
+    {
+        public static IServiceCollection AddDataLayer(this IServiceCollection services, ConfigurationManager configurationManager)
+        {
+            services.AddDbContext<BookStoreDbContext>(options =>
+            {
+                options.UseSqlServer(configurationManager.GetConnectionString("Default"));
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredUniqueChars = 0;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 3;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.User.RequireUniqueEmail = true;
+
+
+            })
+                .AddEntityFrameworkStores<BookStoreDbContext>()
+                .AddDefaultTokenProviders();
+            return services;
+        }
+    }
+}
