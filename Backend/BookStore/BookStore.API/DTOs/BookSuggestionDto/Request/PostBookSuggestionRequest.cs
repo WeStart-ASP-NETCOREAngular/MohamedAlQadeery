4cdb174e609af33ab70774b1848d386ac0643ab5
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-08 00:34:11 +0200

    Added validation in PostBookSuggestionRequest

--- Backend/BookStore/BookStore.API/DTOs/BookSuggestionDto/Request/PostBookSuggestionRequest.cs
+++ Backend/BookStore/BookStore.API/DTOs/BookSuggestionDto/Request/PostBookSuggestionRequest.cs
@@ -1,13 +1,20 @@
-﻿namespace BookStore.API.DTOs.BookSuggestionDto.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.API.DTOs.BookSuggestionDto.Request
 {
     public class PostBookSuggestionRequest
     {
+        [Required]
         public string BookName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
-
+        [Required]
         public string PublisherName { get; set; }
+       [Required]
         public string AuthorName { get; set; }
+       [Required]
         public string Notes { get; set; }
        
     }
