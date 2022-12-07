4559fc857d98bb65f33a6da1413017124fe268cd
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-07 23:20:51 +0200

    created contactus repository

---
+++ Backend/BookStore/BookStore.API/Interfaces/Repositories/IContactusRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.API.Models;
+
+namespace BookStore.API.Interfaces.Repositories
+{
+    public interface IContactusRepository
+    {
+        public Task<Contactus> AddAsync(Contactus contactus);
+        public Task<List<Contactus>> GetAllAsync();
+        public Task<Contactus> GetById(int contactusId);
+
+        public Task<bool> DeleteAsync(int contactusId);
+        public Task<bool> MarkAsRead(int contactusId);
+        public Task<bool> MarkAsUnread(int contactusId);
+    }
+}
---
+++ Backend/BookStore/BookStore.API/Repositories/ContactusRepository.cs
@@ -0,0 +1,64 @@
+﻿using BookStore.API.Data;
+using BookStore.API.Interfaces.Repositories;
+using BookStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Repositories
+{
+    public class ContactusRepository : IContactusRepository
+    {
+        private readonly BookStoreDbContext _context;
+
+        public ContactusRepository(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Contactus> AddAsync(Contactus contactus)
+        {
+          await  _context.Contactus.AddAsync(contactus);
+            await _context.SaveChangesAsync();
+            return contactus;
+        }
+
+        public async Task<bool> DeleteAsync(int contactusId)
+        {
+            var contactus = await _context.Contactus.FindAsync(contactusId);
+            if (contactus == null) return false;
+
+            _context.Contactus.Remove(contactus);
+            return await _context.SaveChangesAsync() >0;
+        }
+
+        public async Task<List<Contactus>> GetAllAsync()
+        {
+            return await _context.Contactus.OrderByDescending(c => c.CreatedAt).ToListAsync();
+        }
+
+        public async Task<Contactus> GetById(int contactusId)
+        {
+            return await _context.Contactus.FirstOrDefaultAsync(c => c.Id == contactusId);
+        }
+
+        public async Task<bool> MarkAsRead(int contactusId)
+        {
+            var contactus = await _context.Contactus.FindAsync(contactusId);
+            if (contactus == null) return false;
+
+            contactus.ReadAt = DateTime.Now;
+             _context.Contactus.Update(contactus);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> MarkAsUnread(int contactusId)
+        {
+            var contactus = await _context.Contactus.FindAsync(contactusId);
+            if (contactus == null) return false;
+
+            contactus.ReadAt = null;
+            _context.Contactus.Update(contactus);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
