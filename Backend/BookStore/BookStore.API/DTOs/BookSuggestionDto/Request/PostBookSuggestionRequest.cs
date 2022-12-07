2d5834b1d591dcc2c43bf947e12084cc90b7674c
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-08 00:23:24 +0200

    Creatin book suggestion dtos

---
+++ Backend/BookStore/BookStore.API/DTOs/BookSuggestionDto/Request/PostBookSuggestionRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStore.API.DTOs.BookSuggestionDto.Request
+{
+    public class PostBookSuggestionRequest
+    {
+        public string BookName { get; set; }
+        public string Email { get; set; }
+
+
+        public string PublisherName { get; set; }
+        public string AuthorName { get; set; }
+        public string Notes { get; set; }
+       
+    }
+}
---
+++ Backend/BookStore/BookStore.API/DTOs/BookSuggestionDto/Response/BookSuggestionResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace BookStore.API.DTOs.BookSuggestionDto.Response
+{
+    public class BookSuggestionResponse
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string Email { get; set; }
+
+
+        public string PublisherName { get; set; }
+        public string AuthorName { get; set; }
+        public string Notes { get; set; }
+        public DateTime? ReadAt { get; set; } 
+        public DateTime CreatedAt { get; set; } 
+    }
+}
