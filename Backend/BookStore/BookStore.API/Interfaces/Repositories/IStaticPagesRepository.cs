5d3d31d4a6d365c7349d1f82a3a162f18a6c8fa8
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-07 22:08:53 +0200

    created static pages repositories

---
+++ Backend/BookStore/BookStore.API/Interfaces/Repositories/IStaticPagesRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.API.Models;
+
+namespace BookStore.API.Interfaces.Repositories
+{
+    public interface IStaticPagesRepository
+    {
+        Task<List<StaticPages>> GetAllAsync();
+        Task<StaticPages> GetByIdAsync(int id);
+        Task<StaticPages> CreateAsync(StaticPages staticPageToCreate);
+        Task<StaticPages> UpdateAsync(int id, StaticPages staticPageToUpdate);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ Backend/BookStore/BookStore.API/Repositories/StaticPagesRepository.cs
@@ -0,0 +1,58 @@
+﻿using BookStore.API.Data;
+using BookStore.API.Interfaces.Repositories;
+using BookStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Repositories
+{
+    public class StaticPagesRepository : IStaticPagesRepository
+    {
+        private readonly BookStoreDbContext _context;
+
+        public StaticPagesRepository(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<StaticPages>> GetAllAsync()
+        {
+            return await _context.StaticPages.ToListAsync();
+        }
+
+        public async Task<StaticPages> GetByIdAsync(int id)
+        {
+            return await _context.StaticPages.FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+
+        public async Task<StaticPages> CreateAsync(StaticPages staticPageToCreate)
+        {
+            await _context.StaticPages.AddAsync(staticPageToCreate);
+            await _context.SaveChangesAsync();
+            return staticPageToCreate;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var staticPage = await _context.StaticPages.FindAsync(id);
+            if (staticPage == null) return false;
+
+            _context.StaticPages.Remove(staticPage);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+
+
+        public async Task<StaticPages> UpdateAsync(int id, StaticPages staticPageToUpdate)
+        {
+            var staticPage = await _context.StaticPages.FirstOrDefaultAsync(a => a.Id == id);
+            if (staticPage == null) return null;
+
+            staticPage.PageName = staticPageToUpdate.PageName;
+            staticPage.Details = staticPageToUpdate.Details;
+            await _context.SaveChangesAsync();
+
+            return staticPage;
+        }
+    }
+}
