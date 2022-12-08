ca1eb2c215342c0877d03c212cacbc8d32569978
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-08 23:16:20 +0200

    create tag/ edit tag from admin panel

--- Frontend/events-client/src/app/admin/components/tags/admin-tag-form/admin-tag-form.component.ts
+++ Frontend/events-client/src/app/admin/components/tags/admin-tag-form/admin-tag-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { TagService } from 'src/app/services/tag.service';
 
 @Component({
   selector: 'app-admin-tag-form',
@@ -6,5 +11,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-tag-form.component.css'],
 })
 export class AdminTagFormComponent implements OnInit {
-  ngOnInit(): void {}
+  tagForm: FormGroup;
+  tagNameInput: FormControl = new FormControl('', Validators.required);
+  id: number | null;
+  formType = 'create';
+  constructor(
+    private _route: ActivatedRoute,
+    private _tagService: TagService,
+    private _router: Router,
+    private _spinner: NgxSpinnerService,
+    private _toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.tagForm = new FormGroup({
+      name: this.tagNameInput,
+    });
+
+    this.TryEditForm();
+  }
+
+  HandleOnSubmit() {
+    console.log(this.tagForm.value);
+    this._spinner.show();
+    if (this.formType == 'create') {
+      this.CreateTag();
+    } else {
+      console.log('Updating tag........ for ' + this.id);
+      this.UpdateTag();
+    }
+  }
+
+  private UpdateTag() {
+    this._tagService.UpdateTag(this.tagForm.value, +this.id!).subscribe({
+      next: (res) => {
+        console.log(res);
+        this._spinner.hide();
+        this._toastr.success(
+          `Tag : ${res.name} has been updated successfully !`
+        );
+
+        this._router.navigate(['admin/tags']);
+      },
+
+      error: (err) => {
+        console.log(err);
+        this._spinner.hide();
+      },
+    });
+  }
+
+  private CreateTag() {
+    this._tagService.CreateTag(this.tagForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+        this._spinner.hide();
+        this._toastr.success(
+          `Tag : ${res.name} has been created successfully !`
+        );
+        this._router.navigate(['admin/tags']);
+      },
+
+      error: (err) => {
+        console.log(err);
+        this._spinner.hide();
+      },
+    });
+  }
+
+  private TryEditForm() {
+    this._route.paramMap.subscribe((param) => {
+      this.id = +param.get('id')!;
+      if (this.id) {
+        this._spinner.show();
+        this._tagService.GetTagById(+this.id).subscribe({
+          next: (tag) => {
+            this.tagForm.controls['name'].setValue(tag.name);
+
+            this.formType = 'Update';
+            this._spinner.hide();
+          },
+
+          error: (err) => {
+            console.log(err);
+            this._router.navigate(['admin/tags']);
+          },
+        });
+      }
+    });
+  }
 }
