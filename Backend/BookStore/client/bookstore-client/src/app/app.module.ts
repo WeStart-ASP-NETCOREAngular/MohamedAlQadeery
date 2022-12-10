32eb9730561e2590067edb5dd73a40e46afed07b
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-10 21:12:58 +0200

    moving card/header/menu/searcBar into there componets

--- Backend/BookStore/client/bookstore-client/src/app/app.module.ts
+++ Backend/BookStore/client/bookstore-client/src/app/app.module.ts
@@ -3,9 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/header/search-bar/search-bar.component';
+import { CartComponent } from './components/header/cart/cart.component';
+import { HeaderComponent } from './components/header/header/header.component';
+import { MenuComponent } from './components/header/menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    SearchBarComponent,
+    CartComponent,
+    HeaderComponent,
+    MenuComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
