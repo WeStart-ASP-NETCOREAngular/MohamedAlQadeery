2f9e20d4334fc886f7d6f6af9db96bc3afbb1a43
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-12 00:12:10 +0200

    created category add-list-update/dashboard component

--- Backend/BookStore/client/bookstore-client/src/app/components/admin/category/category.component.ts
+++ Backend/BookStore/client/bookstore-client/src/app/components/admin/category/category.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { ICategoryResponseDto } from 'src/app/interfaces/category/ICategoryResponseDto';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -8,9 +11,49 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class CategoryComponent implements OnInit {
   formType = 'create';
+  buttonLabel = 'انشاء';
   categoryFormGroup: FormGroup;
   categoryNameInput: FormControl;
-  constructor() {}
+  categoryId: number;
+  categories: ICategoryResponseDto[] = [];
 
-  ngOnInit(): void {}
+  constructor(private _categoryService: CategoryService) {}
+
+  ngOnInit(): void {
+    this.categoryNameInput = new FormControl('', [Validators.required]);
+    this.categoryFormGroup = new FormGroup({ name: this.categoryNameInput });
+
+    this._categoryService.GetAllCategories().subscribe((res) => {
+      this.categories = res;
+    });
+  }
+
+  HandleOnSubmit() {
+    console.log(this.categoryFormGroup.value);
+    if (this.formType == 'create') {
+      this._categoryService
+        .CreateCategory(this.categoryFormGroup.value)
+        .subscribe((res) => {
+          this.categories.push(res);
+        });
+    } else {
+      this._categoryService
+        .UpdateCategory(this.categoryFormGroup.value, this.categoryId)
+        .subscribe((res) => {
+          let category = this.categories.find((c) => c.id == res.id)!;
+          category.name = res.name;
+        });
+    }
+
+    this.categoryFormGroup.reset();
+  }
+
+  OnEdit(categoryId: number) {
+    this._categoryService.GetCategoryById(categoryId).subscribe((res) => {
+      this.formType = 'update';
+      this.buttonLabel = 'تحديث';
+      this.categoryNameInput.setValue(res.name);
+      this.categoryId = res.id;
+    });
+  }
 }
