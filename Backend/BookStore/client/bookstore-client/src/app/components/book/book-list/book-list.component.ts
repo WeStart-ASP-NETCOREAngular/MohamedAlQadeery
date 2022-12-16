a26ca8bfc3db576610ca82628e312cf5d78c4387
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-16 22:51:34 +0200

    displaying books from api in the list

--- Backend/BookStore/client/bookstore-client/src/app/components/book/book-list/book-list.component.ts
+++ Backend/BookStore/client/bookstore-client/src/app/components/book/book-list/book-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IBookResponse } from 'src/app/interfaces/book/IBookResponse';
+import { BookService } from 'src/app/services/book.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.css']
+  styleUrls: ['./book-list.component.css'],
 })
 export class BookListComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private _bookService: BookService) {}
+  imagesUrl = `${environment.baseURL}/images/thumbs/med`;
+  books$: Observable<IBookResponse[]>;
   ngOnInit(): void {
+    this.books$ = this._bookService.GetAllBooks();
   }
-
 }
