96cfa70ece6f06fb526495a1eccf8d9ff7a6ae23
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-22 10:07:59 +0200

    Added admin button in menu to redirect to admin page

--- Backend/BookStore/client/bookstore-client/src/app/components/header/menu/menu.component.ts
+++ Backend/BookStore/client/bookstore-client/src/app/components/header/menu/menu.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, AfterViewChecked {
+  constructor(private _authService: AuthService) {}
 
-  constructor() { }
+  isAdmin = false;
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ngAfterViewChecked(): void {
+    if (this._authService.isUserAdmin()) {
+      this.isAdmin = true;
+    } else {
+      this.isAdmin = false;
+    }
   }
-
 }
