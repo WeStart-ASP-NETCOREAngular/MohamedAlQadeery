9833a6fd8c9d0c890001ead63f677e9bd1633875
MohamedAlQadeery <moh9amad1@gmail.com>
2022-12-24 00:32:30 +0200

    Added button to remove book from faviroate in account page

--- Backend/BookStore/client/bookstore-client/src/app/components/account/favorite-books/favorite-books.component.ts
+++ Backend/BookStore/client/bookstore-client/src/app/components/account/favorite-books/favorite-books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { IBookResponse } from 'src/app/interfaces/book/IBookResponse';
 import { BookService } from 'src/app/services/book.service';
@@ -10,11 +11,29 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./favorite-books.component.css'],
 })
 export class FavoriteBooksComponent implements OnInit {
-  constructor(private _bookService: BookService) {}
+  constructor(
+    private _bookService: BookService,
+    private _toastr: ToastrService
+  ) {}
   favBooks$: Observable<IBookResponse[]>;
   imagesUrl = `${environment.baseURL}/images/thumbs/med`;
 
   ngOnInit(): void {
     this.favBooks$ = this._bookService.GetFavoriteBooks();
+  }
+
+  OnRemoveFromFav(bookId: number) {
+    this._bookService.RemoveBookFromFavorite(bookId).subscribe({
+      next: (res) => {
+        this._toastr.success(
+          'تم ازالة الكتاب من المفضلة بنجاح',
+          'ازالة من المفضلة'
+        );
+        this.favBooks$ = this._bookService.GetFavoriteBooks();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
